b265d1d9cedc9718408cd5a5616b3170c5391da8
Usha Mayuri <ushamayurim@gmail.com>
2024-11-13 17:13:33 +0530

    Initial Commit

---
+++ iOS Assignment/iOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AssignmentApp.swift
+//  iOS Assignment
+//
+//  Created by usha mayuri on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
