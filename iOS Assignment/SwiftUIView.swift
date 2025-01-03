15ea9f51f90578d68dde7900c11fcca15298b551
Usha Mayuri <ushamayurim@gmail.com>
2025-01-03 07:54:48 +0530

    new branch

---
+++ iOS Assignment/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  iOS Assignment
+//
+//  Created by usha mayuri on 03/01/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
